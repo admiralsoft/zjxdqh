c68f87cf17f9cb0608ad1edb5340a8f483783b56
chenyaokun <chenyaokun@sczjxdqh.com>
2019-09-17 15:31:18 +0800

    添加 happyServer 动态数据源配置

---
+++ zjxdqh-sboot-tools/src/main/java/com/zjxdqh/tools/datasource/DataSourceContextHolder.java
@@ -0,0 +1,36 @@
+package com.zjxdqh.tools.datasource;
+
+/**
+ * @author Yorking
+ * @date 2019/09/17
+ */
+
+public class DataSourceContextHolder {
+
+    private static final ThreadLocal<String> contextHolder = new InheritableThreadLocal<>();
+
+    /**
+     * 设置数据源
+     *
+     * @param db
+     */
+    public static void setDataSource(String db) {
+        contextHolder.set(db);
+    }
+
+    /**
+     * 取得当前数据源
+     *
+     * @return
+     */
+    public static String getDataSource() {
+        return contextHolder.get();
+    }
+
+    /**
+     * 清除上下文数据
+     */
+    public static void clear() {
+        contextHolder.remove();
+    }
+}
